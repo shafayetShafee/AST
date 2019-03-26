6cb61e792520ca5ec9034b42d08caf2be5853d0d
shafayetShafee <48413872+shafayetShafee@users.noreply.github.com>
2019-03-27 01:09:35 +0600

    Add files via upload

--- R/r_func2.R
+++ R/r_func2.R
@@ -3,6 +3,7 @@
 #' rowwise and replications will be columnwise
 #'
 #' @param data
+#' @param info if u want all information type "All"
 #'
 #' @return dataframe
 #' @export
@@ -15,23 +16,23 @@
 #' anova_crd(mat)
 
 anova_crd= function(data,info=c("anova","All")){
-  
+
   N = dim(data)[1]*dim(data)[2]
   a = dim(data)[1]
   n = dim(data)[2]
-  
+
   treat_totals = rowSums(data)
   grand_totals = sum(data)
-  
+
   SST_crd = sum(data^2) - (grand_totals^2)/N
   SS_treat_crd = (sum(treat_totals^2))/n - (grand_totals^2)/N
   SS_error_crd = SST_crd - SS_treat_crd
-  
+
   MS_treat_crd = SS_treat_crd/ (a-1)
   MS_error_crd = SS_error_crd / (N-a)
-  
+
   F_ratio_crd = MS_treat_crd/MS_error_crd
-  
+
   if(info=="anova"){
     result = t(data.frame("SST"=SST_crd, "SS_treat" = SS_treat_crd, "SSE" = SS_error_crd,
                           "MS_treatment" = MS_treat_crd, "MSE" = MS_error_crd,
