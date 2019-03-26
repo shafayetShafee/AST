89255dce9526fa02f118fe0d49b1a8b95a3246e6
shafayetShafee <48413872+shafayetShafee@users.noreply.github.com>
2019-03-26 16:43:30 +0600

    Update r_func2.R

--- R/r_func2.R
+++ R/r_func2.R
@@ -15,25 +15,26 @@
 #' anova_crd(mat)
 
 anova_crd= function(data){
-
+  
   N = dim(data)[1]*dim(data)[2]
   a = dim(data)[1]
   n = dim(data)[2]
-
+  
   treat_totals = rowSums(data)
   grand_totals = sum(data)
-
-  SST_crd = sum(obs^2) - (grand_totals^2)/N
-  SS_treat_crd = (sum(treat_totals^2))/b - (grand_totals^2)/N
+  
+  SST_crd = sum(data^2) - (grand_totals^2)/N
+  SS_treat_crd = (sum(treat_totals^2))/n - (grand_totals^2)/N
   SS_error_crd = SST_crd - SS_treat_crd
-
+  
   MS_treat_crd = SS_treat_crd/ (a-1)
   MS_error_crd = SS_error_crd / (N-a)
-
+  
   F_ratio_crd = MS_treat_crd/MS_error_crd
-
+  
   result = t(data.frame("SST"=SST_crd, "SS_treat" = SS_treat_crd, "SSE" = SS_error_crd,
                         "MS_treatment" = MS_treat_crd, "MSE" = MS_error_crd,
+                        "Data_squared" = sum(data^2),
                         "F_ratio" = F_ratio_crd))
   colnames(result) = "values"
   return(result)
