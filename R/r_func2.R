600913783ed825cdf4c52e4acc2beeba44bfd3ce
shafayetShafee <48413872+shafayetShafee@users.noreply.github.com>
2019-03-27 00:57:45 +0600

    Update r_func2.R

--- R/r_func2.R
+++ R/r_func2.R
@@ -14,7 +14,7 @@
 #' mat = matrix(obs, nrow = a, ncol = b, byrow = T)
 #' anova_crd(mat)
 
-anova_crd= function(data){
+anova_rd= function(data,info=c("anova","All")){
   
   N = dim(data)[1]*dim(data)[2]
   a = dim(data)[1]
@@ -32,13 +32,22 @@ anova_crd= function(data){
   
   F_ratio_crd = MS_treat_crd/MS_error_crd
   
-  result = t(data.frame( "grand"=sum(data),
-                         "Data_squared" = sum(data^2),
-                         "treatSquared_by_n"=(sum(treat_totals^2))/n,
-                         "grandSquared_by_N" = (grand_totals^2)/N,
-                         "SST"=SST_crd, "SS_treat" = SS_treat_crd, "SSE" = SS_error_crd,
-                        "MS_treatment" = MS_treat_crd, "MSE" = MS_error_crd,
-                        "F_ratio" = F_ratio_crd))
-  colnames(result) = "values"
-  return(result)
+  if(info=="anova"){
+    result = t(data.frame("SST"=SST_crd, "SS_treat" = SS_treat_crd, "SSE" = SS_error_crd,
+                          "MS_treatment" = MS_treat_crd, "MSE" = MS_error_crd,
+                          "F_ratio" = F_ratio_crd))
+    colnames(result) = "values"
+    return(result)
+  }
+  else if (info=="All"){
+    result = t(data.frame( "grand"=sum(data),
+                           "Data_squared" = sum(data^2),
+                           "treatSquared_by_n"=(sum(treat_totals^2))/n,
+                           "grandSquared_by_N" = (grand_totals^2)/N,
+                           "SST"=SST_crd, "SS_treat" = SS_treat_crd, "SSE" = SS_error_crd,
+                           "MS_treatment" = MS_treat_crd, "MSE" = MS_error_crd,
+                           "F_ratio" = F_ratio_crd))
+    colnames(result) = "values"
+    return(result)
+  }
 }
